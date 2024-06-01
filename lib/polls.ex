2760c2fbcd7105d3f24cd9e9913726001334493a
Ayodeji O <97124713+aosasona@users.noreply.github.com>
2024-06-01 15:51:03 +0100

    Initial commitg

---
+++ lib/polls.ex
@@ -0,0 +1,18 @@
+defmodule Polls do
+  @moduledoc """
+  Documentation for `Polls`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Polls.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
